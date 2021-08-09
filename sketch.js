bb45aacff58990abd9b333b59f163d7c808e27cc
pandurangaur <88498042+pandurangaur@users.noreply.github.com>
2021-08-09 20:42:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,50 +1,18 @@
-var trex, trex_running, edges;
-var groundImage;
-var ground;
+var ship,ship_1,edges;
+var seaImage;
+var sea; 
 function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  groundImage = loadImage("ground2.png")
+ship_1 = addImage("ship_1","ship_2","ship_3","ship-4");
+seaImage = addImage("sea.png");
 }
 
 function setup(){
-  createCanvas(600,200);
-  
-  // creating trex
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("running", trex_running);
-  edges = createEdgeSprites();
-  
-  //adding scale and position to trex
-  trex.scale = 0.5;
-  trex.x = 50
-  //create ground 
-  ground = createSprite(200,180,400,20);
-  ground.addImage("ground",groundImage);
-  
-  ground.velocityX= -2;
+  createCanvas(400,400);
+  //create ship
+  ship=createSprites(50,150,30,20);
 }
 
-
-function draw(){
-  //set background color 
-  background("white");
-  
-  //logging the y position of the trex
-  console.log(trex.y)
-  
-  //jump when space key is pressed
-  if(keyDown("space")){
-    trex.velocityY = -10;
-  }
-  if(ground.x<0){
-    ground.x = ground.width/2;
-  }
-
-
-  trex.velocityY = trex.velocityY + 0.5;
-  
-  //stop trex from falling down
-  trex.collide(ground);
-  
-  drawSprites();
+function draw() {
+  background("blue");
+ 
 }
